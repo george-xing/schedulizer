77316837fae643cc8d273190b8cfacbad07b73fa
george <george@indiegogo.com>
2013-10-11 20:09:35 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Schedulizer::Application.config.secret_key_base = '8a6c135252e42c978183808577eda0bcb69c8f4c36d7befdc8fd3febc78f68b9f3c3203bcbca8174111c0405812314ff652a5e1e75d966129c2f61e2cb22e3dc'
